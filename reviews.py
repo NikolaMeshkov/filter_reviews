4d018c672a67cafe748831fdb8a3e87191d8b6c0
Nikola Meshkov <nikola.meskov1@hotmail.com>
2022-06-25 16:45:01 +0200

    Change var names and add the reverse

--- reviews.py
+++ reviews.py
@@ -8,53 +8,32 @@ import json
 
 
 is_filter_applied = False
-selected_low_high = ""
-selected_min_rating = ""
-selected_by_date = ""
-selected_priority = ""
 
 def apply_filters():
-    
-    is_filter_applied = True
-    window.destroy()
-
-def select_low_high(selected):
-    
-    selected_low_high = selected
-
-def select_min_rating(selected):
-    
-    selected_min_rating = selected
-
-
-def select_by_date(selected):
-    
-    selected_by_date = selected
-
-def select_priority(selected):
-    
-    selected_priority = selected
+	
+	is_filter_applied = True
+	window.destroy()
 
 
 window.geometry('325x250')
 window.configure(background = "white")
 
 
-low_high = StringVar(window)
-low_high.set("") # default value
-w1 = OptionMenu(window, low_high, "Highest first", "Lowest first", command=select_low_high)
+order_by_rating_var = StringVar(window)
+order_by_rating_var.set("Highest first") # default value
+w1 = OptionMenu(window, order_by_rating_var, "Highest first", "Lowest first")
 
 min_rating = StringVar(window)
-min_rating.set('')
-w2 = OptionMenu(window, min_rating, '1', '2', '3', '4', '5', command=select_low_high)
+min_rating.set(1)
+w2 = OptionMenu(window, min_rating, 1, 2, 3, 4, 5)
 
 date_order = StringVar(window)
-date_order.set('')
-w3 = OptionMenu(window, date_order, 'Oldest first', 'Newest first', command=select_by_date)
+date_order.set('Oldest first')
+w3 = OptionMenu(window, date_order, 'Oldest first', 'Newest first')
 
 prioritize_by_text = StringVar(window)
-prioritize_by_text.set('')
-w4 = OptionMenu(window, prioritize_by_text, 'Yes', 'No', command=select_priority)
+prioritize_by_text.set('Yes')
+w4 = OptionMenu(window, prioritize_by_text, 'Yes', 'No')
 w1.pack()
 w2.pack()
 w3.pack()
@@ -69,9 +48,6 @@ button.pack()
 mainloop()
 
 
-
-
-
 json_file = open('reviews.json', 'r', encoding='utf-8-sig')
 
 file_content = json_file.read()
@@ -79,14 +55,22 @@ file_content = json_file.read()
 reviews_list = json.loads(file_content)
 
 
-def filter_reviews(reviews):
+def order_by_has_text(reviews):
+
 	return sorted(reviews, key=lambda review: bool(review['reviewText']), reverse=True) 
 
-def order_by_rating(reviews):
-	return sorted(reviews, key=lambda review: review['rating'], reverse=True)
+def order_by_rating(reviews, selected_order_by_rating1):
+	is_reverse = False
+	if selected_order_by_rating1 == "Highest first":
+		is_reverse = True
+	return sorted(reviews, key=lambda review: review['rating'], reverse=is_reverse)
 
-def order_by_date(reviews):
-	return sorted(reviews, key=lambda review: review['reviewCreatedOnDate'])
+def order_by_date(reviews,selected_by_date1):
+	is_reverse = False
+	if selected_by_date1 == "Highest first":
+		is_reverse = True
+
+	return sorted(reviews, key=lambda review: review['reviewCreatedOnDate'],reverse=is_reverse)
 	
 
 def filter_by_stars(reviews, min_review):
@@ -97,15 +81,13 @@ def filter_by_stars(reviews, min_review):
 	
 	return helper_list
 
+reviews_list = filter_by_stars(reviews_list, int(min_rating.get()))
+reviews_list = order_by_date(reviews_list,date_order.get())
+reviews_list = order_by_rating(reviews_list, order_by_rating_var.get())
 
-reviews_list = filter_by_stars(reviews_list, 3)
-reviews_list = order_by_date(reviews_list)
-reviews_list = order_by_rating(reviews_list)
 
-
-# if selected_priority == 'Yes':
-if True:
-	reviews_list = filter_reviews(reviews_list)
+if prioritize_by_text.get() == 'Yes':
+	reviews_list = order_by_has_text(reviews_list)
 
 print(reviews_list)
 
